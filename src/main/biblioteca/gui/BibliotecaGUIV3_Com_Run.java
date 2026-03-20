4351596b17ef8ce98bc8f864c4560ddfcdaecdf9
Mcintia816 <cintia.alcari@dcx.ufpb.br>
2026-03-20 19:00:40 -0300

    Fix JFrame initialization in BibliotecaGUIV3_Com_Run

--- src/main/biblioteca/gui/BibliotecaGUIV3_Com_Run.java
+++ src/main/biblioteca/gui/BibliotecaGUIV3_Com_Run.java
@@ -6,7 +6,7 @@ public class BibliotecaGUIV3_Com_Run {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                JFrame janela = new BibliotecaGUIV3ComMenu(); // ou BibliotecaGUIV3
+                JFrame janela = new BibliotecaGUIV3ComMenu();
                 janela.setVisible(true);
                 janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             }
